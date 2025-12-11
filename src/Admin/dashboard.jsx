bb9855e630a862cf59b3250418299f72f89300f2
Sebastian <sebastianboterogordo10@gmail.com>
2025-12-10 19:31:26 -0500

    a

--- src/Admin/dashboard.jsx
+++ src/Admin/dashboard.jsx
@@ -412,8 +412,8 @@ const Dashboard = () => {
 
   // Ingredient handlers
 const handleDeleteIngredient = async (id) => {
-  if (window.confirm("¿Estás seguro de eliminar este ingrediente?")) return;
-  
+  if (!window.confirm("¿Estás seguro de eliminar este ingrediente?")) return;
+
   try {
     const res = await fetch(`http://localhost:5000/api/ingredientes/${id}`, {
       method: "DELETE",
